12d6c4e9c266d8f96aef09f90d31601becf2cdca
William McCullough <ZahFoxTales@gmail.com>
2019-01-18 19:10:21 -0600

    finally decided to commit my changes

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -1,12 +1,17 @@
+#include "hzpch.h"
 #include "Application.h"
+#include "Log.h"
 
-
-
+#include <GLFW/glfw3.h>
 
 namespace Hazel {
 
+#define BIND_EVENT_FN(x) std::bind(&x, this, std::placeholders::_1)
+
 	Application::Application()
 	{
+		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(Application::OnEvent));
 	}
 
 
@@ -14,10 +19,48 @@ namespace Hazel {
 	{
 	}
 
-	void Application::Run()
+	void Application::PushLayer(Layer* layer)
 	{
-		while (true);
+		m_LayerStack.PushLayer(layer);
 	}
 
+	void Application::PushOverlay(Layer* layer)
+	{
+		m_LayerStack.PushOverlay(layer);
+	}
+
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(Application::OnWindowClosed));
+
+		for (auto it = m_LayerStack.end(); it != m_LayerStack.begin(); )
+		{
+			(*--it)->OnEvent(e);
+			if (e.Handled)
+				break;
+		}
+	}
+
+	void Application::Run()
+	{
+		while (m_Running)
+		{
+			glClearColor(1, 0, 1, 1);
+			glClear(GL_COLOR_BUFFER_BIT);
+
+			for (Layer* layer : m_LayerStack)
+				layer->OnUpdate();
+
+			m_Window->OnUpdate();
+		}
+	}
+
+
+	bool Application::OnWindowClosed(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
+	}
 }
 
--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -1,6 +1,10 @@
 #pragma once
 
 #include "Core.h"
+#include "Window.h"
+#include "LayerStack.h"
+#include "Events/Event.h"
+#include "Events/ApplicationEvent.h"
 
 namespace Hazel {
 
@@ -11,6 +15,18 @@ namespace Hazel {
 		virtual ~Application();
 
 		void Run();
+
+		void OnEvent(Event& e);
+
+		void PushLayer(Layer* layer);
+
+		void PushOverlay(Layer* layer);
+	private:
+		bool OnWindowClosed(WindowCloseEvent& e);
+
+		std::unique_ptr<Window> m_Window;
+		bool m_Running = true;
+		LayerStack m_LayerStack;
 	};
 
 	// To be defined in the client
