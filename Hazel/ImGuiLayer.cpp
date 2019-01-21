8ae5c4291e7810eab74a3d782e4c17a34458abcf
William McCullough <ZahFoxTales@gmail.com>
2019-01-20 20:43:28 -0600

    feat: got ImGui to display in the client UI

---
+++ Hazel/ImGuiLayer.cpp
@@ -0,0 +1,35 @@
+#include "hzpch.h"
+#include "Hazel/ImGui/ImGuiLayer.h"
+
+namespace Hazel {
+
+	ImGuiLayer::ImGuiLayer()
+		: Layer("ImGuiLayer")
+	{
+	}
+
+	ImGuiLayer::~ImGuiLayer()
+	{
+	}
+
+	void ImGuiLayer::OnAttach()
+	{
+
+	}
+
+	void ImGuiLayer::OnDetach()
+	{
+
+	}
+
+	void ImGuiLayer::OnUpdate()
+	{
+
+	}
+
+	void ImGuiLayer::OnEvent(Event & event)
+	{
+
+	}
+
+}
