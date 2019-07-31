a142f7f9694c72539bbd4e8d7df99d2e9b609d2a
Will <ZahFoxTales@gmail.com>
2019-07-30 20:18:41 -0500

    refactor: slightly changed how the example tilemap renders

--- Hazel/src/Hazel/Renderer/Shader.h
+++ Hazel/src/Hazel/Renderer/Shader.h
@@ -14,6 +14,7 @@ namespace Hazel {
 		void Unbind() const;
 
 		void UploadUniformMat4(const std::string& name, const glm::mat4& matrix) const;
+		void UploadUniformFloat4(const std::string& name, const glm::vec4& values) const;
 	private:
 		uint32_t m_RendererID;
 	};
--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -94,7 +94,7 @@ public:
 
 		m_Shader.reset(new Hazel::Shader(vertexSrc, fragmentSrc));
 
-		const std::string blueShaderVertexSrc = R"(
+		const std::string flatShaderVertexSrc = R"(
 			#version 330 core
 
 			layout(location = 0) in vec3 a_Position;
@@ -111,20 +111,21 @@ public:
 			}
 		)";
 
-		const std::string blueFragmentVertexSrc = R"(
+		const std::string flatColorShaderFragmentSrc = R"(
 			#version 330 core
 
 			layout(location = 0) out vec4 color;
 
 			in vec3 v_Position;
+			uniform vec4 u_Color;
 
 			void main()
 			{
-			  color = vec4(0.2, 0.3, 0.8, 1.0);
+			  color = u_Color;
 			}
 		)";
 
-		m_BlueShader.reset(new Hazel::Shader(blueShaderVertexSrc, blueFragmentVertexSrc));
+		m_FlatColorShader.reset(new Hazel::Shader(flatShaderVertexSrc, flatColorShaderFragmentSrc));
 	}
 
 	void OnUpdate(Hazel::Timestep ts) override
@@ -186,13 +187,25 @@ public:
 
 		const glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
 
+		const glm::vec4 redColor(0.8f, 0.2f, 0.3f, 1.0f);
+		const glm::vec4 blueColor(0.2f, 0.3f, 0.8f, 1.0f);
+
 		for (int i = 0; i < 20; i++)
 		{
 			for (int j = 0; j < 20; j++)
 			{
 				glm::vec3 pos(i*0.11f + m_SquarePosition.x, j*0.11f + m_SquarePosition.y, 0.0f);
 				const glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
-				Hazel::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+				if (j % 2 == 0)
+				{
+					m_FlatColorShader->UploadUniformFloat4("u_Color", redColor);
+				}
+				else
+				{
+					m_FlatColorShader->UploadUniformFloat4("u_Color", blueColor);
+
+				}
+				Hazel::Renderer::Submit(m_FlatColorShader, m_SquareVA, transform);
 			}
 		}
 
@@ -216,7 +229,7 @@ private:
 	std::shared_ptr<Hazel::Shader> m_Shader;
 	std::shared_ptr<Hazel::VertexArray> m_VertexArray;
 
-	std::shared_ptr<Hazel::Shader> m_BlueShader;
+	std::shared_ptr<Hazel::Shader> m_FlatColorShader;
 	std::shared_ptr<Hazel::VertexArray> m_SquareVA;
 
 	Hazel::OrthographicCamera m_Camera;
