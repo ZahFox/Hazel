325e3f7bdb5e8bebaf04f192c4eda584f72146de
Will <ZahFoxTales@gmail.com>
2019-11-08 19:48:49 -0600

    feat: started working on a 2D renderer

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -1,5 +1,4 @@
 #include "Sandbox2D.h"
-#include "Platform/OpenGL/OpenGLShader.h"
 
 #include "imgui/imgui.h"
 
@@ -12,35 +11,10 @@ Sandbox2D::Sandbox2D() : Layer("Sandbox2D"), m_CameraController(1280.0f / 720.0f
 
 void Sandbox2D::OnAttach()
 {
-	float squareVertices[4 * 3] = {
-	-0.5f, -0.5f, 0.0f,
-	 0.5f, -0.5f, 0.0f,
-	 0.5f,  0.5f, 0.0f,
-	-0.5f,  0.5f, 0.0f,
-	};
-
-	m_SquareVA = Hazel::VertexArray::Create();
-	Hazel::Ref<Hazel::VertexBuffer> squareVB;
-	squareVB.reset(Hazel::VertexBuffer::Create(squareVertices, sizeof(squareVertices)));
-
-	squareVB->SetLayout(
-		{
-			{Hazel::ShaderDataType::Float3, "a_Position"},
-		}
-	);
-
-	m_SquareVA->AddVertexBuffer(squareVB);
-	unsigned int squareIndices[6] = { 0, 1, 2, 2, 3, 0 };
-	Hazel::Ref<Hazel::IndexBuffer> squareIB;
-	squareIB.reset(Hazel::IndexBuffer::Create(squareIndices, sizeof(squareIndices) / sizeof(uint32_t)));
-	m_SquareVA->SetIndexBuffer(squareIB);
-
-	m_FlatColorShader = Hazel::Shader::Create("assets/shaders/FlatColor.glsl");
 }
 
 void Sandbox2D::OnDetach()
 {
-	
 }
 
 void Sandbox2D::OnUpdate(Hazel::Timestep ts)
@@ -50,13 +24,9 @@ void Sandbox2D::OnUpdate(Hazel::Timestep ts)
 
 	m_CameraController.OnUpdate(ts);
 
-	Hazel::Renderer::BeginScene(m_CameraController.GetCamera());
-
-	std::dynamic_pointer_cast<Hazel::OpenGLShader>(m_FlatColorShader)->Bind();
-	std::dynamic_pointer_cast<Hazel::OpenGLShader>(m_FlatColorShader)->UploadUniformFloat4("u_Color", m_SquareColor);
-	Hazel::Renderer::Submit(m_FlatColorShader, m_SquareVA, glm::scale(glm::mat4(1.0f), glm::vec3(1.5f)));
-	
-	Hazel::Renderer::EndScene();
+	Hazel::Renderer2D::BeginScene(m_CameraController.GetCamera());
+	Hazel::Renderer2D::DrawQuad({0.0f, 0.0f}, {1.0f, 1.0f}, {0.8f, 0.2f, 0.3f, 1.0f});
+	Hazel::Renderer2D::EndScene();
 }
 
 void Sandbox2D::OnImGuiRender()
