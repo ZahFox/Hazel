763237ae33ba39cc64c211eb2cc8ae5e9dfd04ef
Will <ZahFoxTales@gmail.com>
2019-12-19 19:58:01 -0600

    feat: made it possible to have shaders with a texture and a color

--- Hazel/src/Platform/OpenGL/OpenGLTexture.h
+++ Hazel/src/Platform/OpenGL/OpenGLTexture.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Hazel/Renderer/Texture.h"
+#include <glad/glad.h>
 
 namespace Hazel
 {
@@ -8,10 +9,14 @@ namespace Hazel
 	{
 	public:
 		OpenGLTexture2D(const std::string& path);
+		OpenGLTexture2D(uint32_t width, uint32_t height);
+		
 		virtual ~OpenGLTexture2D();
 
 		virtual uint32_t GetWidth() const override { return m_Width; };
 		virtual uint32_t GetHeight() const override { return m_Height; };
+		
+		virtual void SetData(void* data, uint32_t size) override;
 
 		virtual void Bind(uint32_t slot = 0) const override;
 
@@ -20,5 +25,6 @@ namespace Hazel
 		uint32_t m_Width;
 		uint32_t m_Height;
 		uint32_t m_RendererID;
+		GLenum m_InternalFormat, m_DataFormat;
 	};
 }
